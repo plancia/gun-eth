23f8b941b3b8eaf9a8cfd4f2e0113817664f4e22
unknown <scobru1988@gmail.com>
2024-12-10 23:09:42 +0100

    new version 3.0.0

--- src/browser.js
+++ src/browser.js
@@ -1,17 +1,15 @@
 /**
  * @typedef {Object} BrowserGunEth
  * @property {typeof import('./core/gun-eth.js').GunEth} GunEth
- * @property {typeof import('./features/proof/ProofChain.js').ProofChain} ProofChain
  */
 
 // Import only browser-required modules
 import { GunEth } from './core/gun-eth.js';
-import { ProofChain } from './features/proof/ProofChain.js';
+
 
 /** @type {BrowserGunEth} */
 const browserGunEth = {
-  GunEth,
-  ProofChain
+  GunEth
 };
 
 export default browserGunEth;
