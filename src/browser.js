d2ce1d11a8c04d31ec7085c18d551445e9f60041
unknown <scobru1988@gmail.com>
2024-12-11 15:36:55 +0100

    bug fix authentication ethToGunAccount

--- src/browser.js
+++ src/browser.js
@@ -3,9 +3,7 @@
  * @property {typeof import('./core/gun-eth.js').GunEth} GunEth
  */
 
-// Import only browser-required modules
 import { GunEth } from './core/gun-eth.js';
-
 
 /** @type {BrowserGunEth} */
 const browserGunEth = {
